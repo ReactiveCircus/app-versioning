b546bb0723975f1702a382fc2310a43c2385dd17
Yang <ychescale9@gmail.com>
2020-09-23 09:20:31 +1000

    AGP 4.2.0-alpha12.

--- src/functionalTest/kotlin/io/github/reactivecircus/appversioning/fixtures/ProjectTemplates.kt
+++ src/functionalTest/kotlin/io/github/reactivecircus/appversioning/fixtures/ProjectTemplates.kt
@@ -31,7 +31,7 @@ fun gradlePropertiesFileContent(enableConfigurationCache: Boolean): String {
         """.trimIndent()
     } else ""
     return """
-        org.gradle.unsafe.watch-fs=true
+        org.gradle.vfs.watch=true
         $configurationCacheProperties
     """.trimIndent()
 }
