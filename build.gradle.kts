005d381bd82de0a927b6930a078e0bd51d47ef27
Yang <reactivecircus@gmail.com>
2020-12-24 21:24:04 +1100

    Target Java 11 bytecode.

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,16 +53,16 @@ gradlePlugin {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 kotlin {
     target {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "1.8"
-                freeCompilerArgs = freeCompilerArgs + listOf(
+                jvmTarget = "11"
+                freeCompilerArgs += listOf(
                     "-Xjvm-default=all",
                     "-Xinline-classes",
                     "-Xopt-in=kotlin.Experimental"
