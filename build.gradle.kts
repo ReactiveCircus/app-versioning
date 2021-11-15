dcfaea497e3cd818451ef04f566a8f903ee06656
Yang <reactivecircus@gmail.com>
2021-11-15 19:38:08 +1100

    Kotlin 1.6.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ object versions {
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
-    kotlin("jvm") version "1.5.31"
+    kotlin("jvm") version "1.6.0"
     id("com.gradle.plugin-publish") version "0.12.0"
     id("com.vanniktech.maven.publish") version "0.17.0"
     id("io.gitlab.arturbosch.detekt") version "1.18.1"
