7b23e75096e9b902895f8392a58c6eae6c1ce802
Yang <reactivecircus@gmail.com>
2022-01-26 10:16:38 +1100

    Compile and test with AGP 7.1.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 @Suppress("ClassName")
 object versions {
-    const val agp = "7.0.4"
-    const val agpCommon = "30.0.4"
+    const val agp = "7.1.0"
+    const val agpCommon = "30.1.0"
     const val detekt = "1.19.0"
     const val junit = "4.13.1"
     const val truth = "1.1.3"
