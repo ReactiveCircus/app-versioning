7946692a517db30279c54634d82a100278403676
Yang <reactivecircus@gmail.com>
2022-07-18 18:01:52 +1000

    Detekt 1.21.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 object versions {
     const val agp = "7.2.1"
     const val agpCommon = "30.2.1"
-    const val detekt = "1.20.0"
+    const val detekt = "1.21.0"
     const val junit = "4.13.1"
     const val truth = "1.1.3"
 }
