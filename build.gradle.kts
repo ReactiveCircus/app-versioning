09df2827ce74673459e5a9fac7a10f6df1beaaa7
Yang <ychescale9@gmail.com>
2022-10-21 16:26:27 +1100

    Binary compatibility validator 0.12.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
     id("com.gradle.plugin-publish") version "0.12.0"
     id("com.vanniktech.maven.publish") version "0.22.0"
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
-    id("binary-compatibility-validator") version "0.11.1"
+    id("binary-compatibility-validator") version "0.12.0"
     id("com.autonomousapps.plugin-best-practices-plugin") version "0.2"
 }
 
